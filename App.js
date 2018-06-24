c038bb140e2033803a8c34a725d6850403a84560
Abhishek Anand <abhishekanand524@gmail.com>
2018-06-24 15:06:33 +0530

    Basic version completed

--- App.js
+++ App.js
@@ -11,30 +11,17 @@ import {
   Text,
   View
 } from 'react-native';
+import Header from './src/compnents/header'
+import AlbumList from './src/compnents/AlbumList'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={{ flex: 1}}>
+        <Header headerText={'Albums'}/>
+        <AlbumList/>
       </View>
-    );
+      );
   }
 }
 
---
+++ src/compnents/CardSection.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  StyleSheet,
+  View
+} from 'react-native';
+
+
+export default class CardSection extends Component {
+    render() {
+        const { viewStyle, textStyle } = styles;
+      return (
+      <View style={styles.containerStyle}>
+        {this.props.children}
+      </View>
+      );
+    }
+} 
+
+
+const styles = {
+    containerStyle: {
+        borderBottomWidth: 1,
+        padding:5,
+        backgroundColor:'#fff',
+        justifyContent:'flex-start',
+        flexDirection: 'row',
+        borderColor:'#ddd',
+        position: 'relative'
+    }
+};
